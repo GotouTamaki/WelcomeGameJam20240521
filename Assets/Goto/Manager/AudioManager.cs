e717ba6ac746aa7e5b318891bc36a8b196b91906
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2024-05-21 14:46:20 +0900

    [update]AudioManagerの更新

--- Assets/Goto/Manager/AudioManager.cs
+++ Assets/Goto/Manager/AudioManager.cs
@@ -1,20 +1,53 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager Instance;
+    
     [SerializeField] AudioMixer _audioMixer;
+    [SerializeField] AudioSource _bgmAudioSource;
+    [SerializeField] AudioSource _seAudioSource;
+    [SerializeField] List<BGMSoundData> _bgmSoundData;
+    [SerializeField] List<SESoundData> _seSoundData;
     [SerializeField] Slider _masterSlider;
     [SerializeField] Slider _bgmSlider;
     [SerializeField] Slider _seSlider;
 
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    
     void Start()
     {
         //スライダーを動かした時の処理を登録。Startでないとうまくいかない。
         _masterSlider.onValueChanged.AddListener(SetAudioMixerMasterVolume);
         _bgmSlider.onValueChanged.AddListener(SetAudioMixerBGMVolume);
         _seSlider.onValueChanged.AddListener(SetAudioMixerSEVolume);
+    }
+
+    public void PlayBGM(BGMSoundData.BGM bgm)
+    {
+        BGMSoundData data = _bgmSoundData.Find(data => data.bgm == bgm);
+        _bgmAudioSource.clip = data.audioClip;
+        _bgmAudioSource.Play();
+    }
+
+    public void PlaySE(SESoundData.SE se)
+    {
+        SESoundData data = _seSoundData.Find(data => data.se == se);
+        _seAudioSource.PlayOneShot(data.audioClip);
     }
 
     /// <summary>AudioMixerでMasterの音量をセットします</summary>
@@ -52,4 +85,35 @@ public class AudioManager : MonoBehaviour
         decibel = Mathf.Clamp(decibel, -80f, 0f);
         _audioMixer.SetFloat("SEVolume", decibel);
     }
+}
+
+[System.Serializable]
+public class BGMSoundData
+{
+    // これがラベルになる
+    public enum BGM
+    {
+        Title,
+        InGame,
+    }
+
+    public BGM bgm;
+    public AudioClip audioClip;
+    [Range(0, 1)] public float volume = 1;
+}
+
+[System.Serializable]
+public class SESoundData
+{
+    // これがラベルになる
+    public enum SE
+    {
+        Attack,
+        Damage,
+        GetItem,
+    }
+
+    public SE se;
+    public AudioClip audioClip;
+    [Range(0, 1)] public float volume = 1;
 }
